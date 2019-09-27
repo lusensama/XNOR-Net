ba9ce67f70346d0c6f60df6430dffab02c2ca38b
lusen <lusensama@gmail.com>
2019-09-27 18:23:08 -0400

    updated network structure

--- models/alexnet.lua
+++ models/alexnet.lua
@@ -12,16 +12,16 @@ function createModel()
    end
 
 local features = nn.Sequential()
-   features:add(ContConvolution(3,96,11,11,4,4,2,2))       -- 224 -> 55
-   features:add(MaxPooling(3,3,2,2))                   -- 55 ->  27
-   features:add(ContConvolution(96,256,5,5,1,1,2,2))       --  27 -> 27  
-   features:add(MaxPooling(3,3,2,2))                     --  27 ->  13
-   features:add(ContConvolution(256,384,3,3,1,1,1,1))      --  13 ->  13
-   features:add(ContConvolution(384,384,3,3,1,1,1,1)) 
+   features:add(ContConvolution(3,64,3,3,2,2,1,1))       -- 224 -> 55
+   features:add(MaxPooling(2,2,1,1))                   -- 55 ->  27
+   features:add(ContConvolution(64,192,3,3,1,1,1,1))       --  27 -> 27  
+   features:add(MaxPooling(2,2,1,1))                     --  27 ->  13
+   features:add(ContConvolution(192,384,3,3,1,1,1,1))      --  13 ->  13
    features:add(ContConvolution(384,256,3,3,1,1,1,1)) 
-   features:add(MaxPooling(3,3,2,2))           
+   features:add(ContConvolution(256,256,3,3,1,1,1,1)) 
+   features:add(MaxPooling(2,2,1,1))           
    features:add(nn.SpatialDropout(opt.dropout))
-   features:add(ContConvolution(256,4096,6,6))
+   features:add(ContConvolution(256,4096,2,2))
    features:add(nn.SpatialDropout(opt.dropout))           
    features:add(ContConvolution(4096,4096,1,1)) 
    features:add(cudnn.SpatialConvolution(4096, nClasses,1,1))

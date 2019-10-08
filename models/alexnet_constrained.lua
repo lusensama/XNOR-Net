fe15ec77db54a0056262d2cd2ed6f3eafc4115e7
lusensama <lusensama@gmail.com>
2019-10-08 15:02:23 -0400

    Update alexnet_constrained.lua

--- models/alexnet_constrained.lua
+++ models/alexnet_constrained.lua
@@ -2,7 +2,7 @@ function createModel()
    require 'cudnn'
    local function ContConvolution(nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH)
          local C= nn.Sequential()
-          C:add(cudnn.SpatialConvolution(nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH):noBias())   
+          C:add(cudnn.SpatialConvolution(nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH))   
           
 --           C:add(cudnn.noBias())                                     -- no bias
          --  C:add(nn.SpatialBatchNormalization(nOutputPlane,1e-3)) -- dropout
@@ -10,7 +10,7 @@ function createModel()
           return C
    end
    local function AveragePooling(kW, kH, dW, dH, padW, padH)
-    return nn.SpatialMaxPooling(kW, kH, dW, dH, padW, padH)
+    return nn.SpatialAveragePooling(kW, kH, dW, dH, padW, padH)
    end
 
 local features = nn.Sequential()
@@ -29,7 +29,7 @@ local features = nn.Sequential()
    features:add(nn.SpatialDropout(opt.dropout))           
    features:add(ContConvolution(4096,4096,1,1))           
    features:add(nn.SpatialDropout(opt.dropout))            -- regularize?
-   features:add(cudnn.SpatialConvolution(4096, nClasses,1,1):noBias())
+   features:add(cudnn.SpatialConvolution(4096, nClasses,1,1))
 --    features:add(cudnn.noBias())                           -- no bias
    features:add(nn.View(nClasses))
    features:add(nn.LogSoftMax())
